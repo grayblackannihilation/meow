b7159b91e6e51000a007f82ea8b6e5842ad4f192
era3effulgence <123359284+grayblackannihilation@users.noreply.github.com>
2023-08-26 17:56:50 +0800

    1

--- js/plugins/skiptitle.js
+++ js/plugins/skiptitle.js
@@ -1,19 +1,3 @@
-//=============================================================================
-// 跳過標題.js
-//=============================================================================
-
-/*:
- * @plugindesc Skip Title.
- * @author taroxd
- *
- * @param Test Only
- * @desc Whether to skip title only in playtest. true/false
- * @type boolean
- * @default true
- *
- * @help This plugin does not provide plugin commands. RPG Maker MZ is not supported.
- */
-
 void function() {
 
     var parameters = PluginManager.parameters('SkipTitle');
@@ -23,11 +7,18 @@ void function() {
     if (enable) {
         Scene_Boot.prototype.start = function() {
             Scene_Base.prototype.start.call(this);
-            SoundManager.preloadImportantSounds();
             this.checkPlayerLocation();
-            DataManager.setupNewGame();
+            
+            // 檢查存檔1是否存在
+            if (DataManager.isThisGameFile(1)) {
+                // 載入存檔1的遊戲狀態
+                DataManager.loadGame(1);
+            } else {
+                // 開始新遊戲
+                DataManager.setupNewGame();
+            }
+            
             SceneManager.goto(Scene_Map);
         };
     }
-
 }();
